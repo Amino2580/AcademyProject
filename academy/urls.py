8dfcf732e2d4877ba708ac4cff5bed4d1cf9120c
Amino2580 <jalilpouramin4@gmail.com>
2026-08-19 11:03:31 +0330

    Remove old index html template

--- academy/urls.py
+++ academy/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("", include("core.urls")),
+    
     path("admin/", admin.site.urls),
     path("teachers/", include("teachers.urls")),
     path("courses/", include("courses.urls")),
